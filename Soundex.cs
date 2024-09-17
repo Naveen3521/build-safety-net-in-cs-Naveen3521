f5085ef83eafbafdf3cde8969d69a458057c4894
Naveen3521 <87026171+Naveen3521@users.noreply.github.com>
2024-09-17 18:32:54 +0530

    Update Soundex.cs

--- Soundex.cs
+++ Soundex.cs
@@ -1,69 +1,75 @@
 using System;
 using System.Text;
+
 public class Soundex
 {
     public static string GenerateSoundex(string name)
     {
         if (string.IsNullOrEmpty(name))
         {
-            return string.Empty;
+            return "0000";
         }
+
+        // Initialize the Soundex code with the first character
         StringBuilder soundex = new StringBuilder();
         soundex.Append(char.ToUpper(name[0]));
+
         char prevCode = GetSoundexCode(name[0]);
+
+        // Append Soundex codes from the rest of the characters
         AppendSoundexCodes(name, soundex, ref prevCode);
+
+        // Pad the Soundex code to ensure it is exactly 4 characters long
         PadSoundexString(soundex);
+
         return soundex.ToString();
     }
 
     private static void AppendSoundexCodes(string name, StringBuilder soundex, ref char prevCode)
     {
-       for (int i = 1; NameAndSoundexValidator(name,soundex); i++)
-       {
-         char code = GetSoundexCode(name[i]);
-         if (ShouldAppendCode(code, prevCode))
-         {
-            soundex.Append(code);
-            prevCode = code;
-         }
-      }
+        for (int i = 1; i < name.Length; i++)
+        {
+            char code = GetSoundexCode(name[i]);
+            if (ShouldAppendCode(code, prevCode))
+            {
+                soundex.Append(code);
+                prevCode = code;
+            }
+        }
     }
     
-   private static bool NameAndSoundexValidator(string name,StringBuilder soundex)
-   {
-       return name.Length<4 && soundex.Length<4;
-   }
-   private static bool ShouldAppendCode(char code, char prevCode)
-   {
-       return code != '0' && code != prevCode;
-   }
-
-   private static void PadSoundexString(StringBuilder soundex)
-   {
-      while (soundex.Length < 4)
-      {
-        soundex.Append('0');
-      }
-   }
-   
-    private static readonly (string Characters,char Code)[] SoundexGroups = 
+    private static bool ShouldAppendCode(char code, char prevCode)
     {
-       ("BFPV", '1'),
-       ("CGJKQSXZ", '2'),
-       ("DT", '3'),
-       ("L", '4'),
-       ("MN", '5'),
-       ("R", '6')
+        return code != '0' && code != prevCode;
+    }
+
+    private static void PadSoundexString(StringBuilder soundex)
+    {
+        while (soundex.Length < 4)
+        {
+            soundex.Append('0');
+        }
+    }
+    
+    private static readonly (string Characters, char Code)[] SoundexGroups = 
+    {
+        ("BFPV", '1'),
+        ("CGJKQSXZ", '2'),
+        ("DT", '3'),
+        ("L", '4'),
+        ("MN", '5'),
+        ("R", '6')
     };
+    
     private static char GetSoundexCode(char c)
     {
         c = char.ToUpper(c);
         foreach (var group in SoundexGroups)
         {
-           if (group.Characters.Contains(c))
-           {
-             return group.Code;
-           }
+            if (group.Characters.Contains(c))
+            {
+                return group.Code;
+            }
         }
         return '0';
     }
