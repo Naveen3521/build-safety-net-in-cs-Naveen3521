614c8dd342a755d1773333b4a8edaafe7f8216b9
Naveen3521 <87026171+Naveen3521@users.noreply.github.com>
2024-09-18 21:11:40 +0530

    Update Soundex.Tests.cs

--- Soundex.Tests.cs
+++ Soundex.Tests.cs
@@ -5,8 +5,7 @@ public class SoundexTests
     [Theory]
     [InlineData("", "0000")]
     [InlineData("   ", "0000")]
-    [InlineData(null, "0000")]
-    public void HandlesEmptyString_NullString_WhitespaceString(string name, string expectedValue)
+    public void GenerateSoundex_HandlesEmptyStringWhitespaceString(string name, string expectedValue)
     {
         // Act
         string actual = Soundex.GenerateSoundex(name);
@@ -19,7 +18,7 @@ public class SoundexTests
     [InlineData("A", "A000")]
     [InlineData("BF", "B100")]
     [InlineData("CPG", "C120")]
-    public void HandlesCharacterLessThanFour(string input, string expected)
+    public void GenerateSoundex_HandlesCharacterLessThanFour(string input, string expected)
     {
         // Act
         string actual = Soundex.GenerateSoundex(input);
@@ -31,7 +30,7 @@ public class SoundexTests
     [Theory]
     [InlineData("Robert", "R163")]
     [InlineData("robeRT", "R163")]
-    public void HandlesCaseSensitiveCharacters(string name, string expectedSoundex)
+    public void GenerateSoundex_HandlesCaseSensitiveCharacters(string name, string expectedSoundex)
     {
         // Act
         string actualSoundex = Soundex.GenerateSoundex(name);
@@ -43,7 +42,7 @@ public class SoundexTests
     [Theory]
     [InlineData("1234", "0000")]
     [InlineData("#$% ^&", "0000")]
-    public void HandlesOnlyNonAlphabeticCharacters(string name, string expectedSoundex)
+    public void GenerateSoundex_HandlesOnlyNonAlphabeticCharacters(string name, string expectedSoundex)
     {
         // Act
         string actualSoundex = Soundex.GenerateSoundex(name);
@@ -55,7 +54,7 @@ public class SoundexTests
     [Theory]
     [InlineData("J@hn", "J500")]
     [InlineData("Alex1nderSmith", "A425")]
-    public void HandlesNameWithSpecialCharacters(string name, string expectedSoundex)
+    public void GenerateSoundex_HandlesNameWithSpecialCharacters(string name, string expectedSoundex)
     {
         // Act
         string actualSoundex = Soundex.GenerateSoundex(name);
@@ -67,7 +66,7 @@ public class SoundexTests
     [Theory]
     [InlineData("Aaaa", "A000")]
     [InlineData("Danna", "D500")]
-    public void HandlesNamesWithRepeatedCharacters(string name, string expectedSoundex)
+    public void GenerateSoundex_HandlesNamesWithRepeatedCharacters(string name, string expectedSoundex)
     {
         // Act
         string actualSoundex = Soundex.GenerateSoundex(name);
@@ -80,7 +79,7 @@ public class SoundexTests
     [InlineData("AEIOU", "A000")]
     [InlineData("I", "I000")]
     [InlineData("EAOUI", "E000")]
-    public void HandlesNamesWithAllVowels(string name, string expectedSoundex)
+    public void GenerateSoundex_HandlesNamesWithAllVowels(string name, string expectedSoundex)
     {
         // Act
         string actualSoundex = Soundex.GenerateSoundex(name);
@@ -94,7 +93,7 @@ public class SoundexTests
     [InlineData("Uri", "U650")]      
     [InlineData("Oce", "O020")]  
     [InlineData("ABACAB", "A120")]
-    public void HandlesNamesWithConsonantAndVowels(string name, string expectedSoundex)
+    public void GenerateSoundex_HandlesNamesWithConsonantAndVowels(string name, string expectedSoundex)
     {
         // Act
         string actualSoundex = Soundex.GenerateSoundex(name);
@@ -107,7 +106,7 @@ public class SoundexTests
     [InlineData(" Samuel", "S540")]
     [InlineData("Samuel ", "S540")]
     [InlineData("Sam uel", "S540")]
-    public void HandlesSpacesBetweenString(string name, string expectedSoundex)
+    public void GenerateSoundex_HandlesSpacesBetweenString(string name, string expectedSoundex)
     {
         // Act
         string actualSoundex = Soundex.GenerateSoundex(name);
@@ -119,7 +118,7 @@ public class SoundexTests
     [Theory]
     [InlineData("Robert", "R163")]
     [InlineData("Alex", "A420")] 
-    public void HandlesCorrectEntry(string name, string expectedSoundex)
+    public void GenerateSoundex_HandlesCorrectEntry(string name, string expectedSoundex)
     {
         // Act
         string actualSoundex = Soundex.GenerateSoundex(name);
